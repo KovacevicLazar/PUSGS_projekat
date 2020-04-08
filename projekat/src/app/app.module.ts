799857ff27426f1772a57e5dcf88ae542f73e964
ilijaicocelic <icocelic@gmail.com>
2020-04-08 21:27:46 +0200

    starting-page

--- projekat/src/app/app.module.ts
+++ projekat/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './componente/nav-menu/nav-menu/nav-menu.component';
 import { SignInComponent } from './componente/sign-in/sign-in/sign-in.component';
 import { SignUpComponent } from './componente/sign-up/sign-up/sign-up.component';
+import { NonRegistredComponent } from './componente/non-registred/non-registred/non-registred.component';
+import { StartingPageComponent } from './componente/starting-page/starting-page/starting-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    NonRegistredComponent,
+    StartingPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ projekat/src/app/componente/non-registred/non-registred/non-registred.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-non-registred',
+  templateUrl: './non-registred.component.html',
+  styleUrls: ['./non-registred.component.css']
+})
+export class NonRegistredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
