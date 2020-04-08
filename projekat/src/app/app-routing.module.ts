0db3786e891cbd3c550b1309733648c29691bc02
KovacevicLazar <lazarkovacevic7@gmail.com>
2020-04-08 23:41:36 +0200

    prikaz profila korisnika

--- projekat/src/app/app-routing.module.ts
+++ projekat/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './componente/profile/profile.component';
+import { FriendsComponent } from './componente/friends/friends.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "profile",
+    component: ProfileComponent
+  },
+  {
+    path: "friends",
+    component: FriendsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
